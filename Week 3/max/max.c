eca2bdc89e34921255d852e504307a85c7333d86
Kyle Español <kylekce@gmail.com>
2023-02-04 08:58:18 +0800

    Add files via upload

---
+++ Week 3/max/max.c
@@ -0,0 +1,39 @@
+// Practice writing a function to find a max value
+
+#include <cs50.h>
+#include <stdio.h>
+
+int max(int array[], int n);
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Number of elements: ");
+    }
+    while (n < 1);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = get_int("Element %i: ", i);
+    }
+
+    printf("The max value is %i.\n", max(arr, n));
+}
+
+// TODO: return the max value
+int max(int array[], int n)
+{
+    int max = array[0]; // place holder for max value
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i]; // update max value
+        }
+    }
+    return max;
+}
