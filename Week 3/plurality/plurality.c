e3bbe2d43d001169fc85b92045eecd39a3524aa5
Kyle Español <kylekce@gmail.com>
2023-02-05 18:39:13 +0800

    Add files via upload

---
+++ Week 3/plurality/plurality.c
@@ -0,0 +1,121 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Max number of candidates
+#define MAX 9
+
+// Candidates have name and vote count
+typedef struct
+{
+    string name;
+    int votes;
+}
+candidate;
+
+// Array of candidates
+candidate candidates[MAX];
+
+// Number of candidates
+int candidate_count;
+
+// Function prototypes
+bool vote(string name);
+void print_winner(void);
+
+int main(int argc, string argv[])
+{
+    // Check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+
+    // Populate array of candidates
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].votes = 0;
+    }
+
+    int voter_count = get_int("Number of voters: ");
+
+    // Loop over all voters
+    for (int i = 0; i < voter_count; i++)
+    {
+        string name = get_string("Vote: ");
+
+        // Check for invalid vote
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
+
+    // Display winner of election
+    print_winner();
+}
+
+// Update vote totals given a new vote
+bool vote(string name)
+{
+    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(name, candidates[i].name) == 0)
+        {
+            candidates[i].votes += 1;
+            return true;
+        }
+    }
+    return false;
+}
+
+// Print the winner (or winners) of the election
+void print_winner(void)
+{
+    // TODO
+    // bubble sort
+    string tempName;
+    int tempVotes;
+
+    for (int i = 0; i < candidate_count - 1; i++)
+    {
+        for (int j = 0; j < candidate_count - i - 1; j++)
+        {
+            if (candidates[j].votes < candidates[j + 1].votes)
+            {
+                // placeholder
+                tempName = candidates[j].name;
+                tempVotes = candidates[j].votes;
+
+                // swap
+                candidates[j].name = candidates[j + 1].name;
+                candidates[j].votes = candidates[j + 1].votes;
+
+                // copy placeholder to j + 1
+                candidates[j + 1].name = tempName;
+                candidates[j + 1].votes = tempVotes;
+            }
+        }
+    }
+
+    // calls the first element
+    printf("%s\n", candidates[0].name);
+    // calls the next elements if tied to the first element
+    for (int i = 1; i < candidate_count; i++)
+    {
+        if (candidates[0].votes == candidates[i].votes)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+    }
+    return;
+}
