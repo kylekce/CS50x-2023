194cf2169b66417618b8cedce6ef1f517c9d35bb
Kyle Español <kylekce@gmail.com>
2023-02-08 11:53:06 +0800

    Add files via upload

---
+++ Week 4/license/license.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    // Check for command line args
+    if (argc != 2)
+    {
+        printf("Usage: ./read infile\n");
+        return 1;
+    }
+
+    // Create buffer to read into
+    char buffer[7];
+
+    // Create array to store plate numbers
+    char *plates[8];
+
+    FILE *infile = fopen(argv[1], "r");
+
+    int idx = 0;
+
+    while (fread(buffer, 1, 7, infile) == 7)
+    {
+        // Replace '\n' with '\0'
+        buffer[6] = '\0';
+
+        // Save plate number in array
+        plates[idx] = malloc(7);
+        strcpy(plates[idx], buffer);
+        idx++;
+    }
+
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%s\n", plates[i]);
+        free(plates[i]);
+    }
+}
